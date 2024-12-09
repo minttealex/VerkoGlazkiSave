3e3add098139467f62e822aeb0857874a70bb3ce
Alexandra Verko <minttealex@gmail.com>
2024-12-09 20:09:11 +0500

    11 лаба (в процессе)

--- VerkoGlazkiSave/AgentPage.xaml.cs
+++ VerkoGlazkiSave/AgentPage.xaml.cs
@@ -23,11 +23,73 @@ namespace VerkoGlazkiSave
         public AgentPage()
         {
             InitializeComponent();
+
+            var currentAgents = ВеркоГлазкиSaveEntities.GetContext().Agent.ToList();
+
+            AgentListView.ItemsSource = currentAgents;
+
+            ComboFilter.SelectedIndex = 0;
+            ComboSorting.SelectedIndex = 0;
+
+            UpdateAgents();
+
+            // Manager.MainFrame.Navigate(new AddEditPage());
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void UpdateAgents()
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            var currentAgents = ВеркоГлазкиSaveEntities.GetContext().Agent.ToList();
+
+            currentAgents = currentAgents.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Email.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Phone.Contains(TBoxSearch.Text)).ToList();
+            
+            if (ComboSorting.SelectedIndex == 1)
+            {
+                currentAgents = currentAgents.OrderBy(p => p.Title).ToList();
+            }
+            if (ComboSorting.SelectedIndex == 2)
+            {
+                currentAgents = currentAgents.OrderByDescending(p => p.Title).ToList();
+            }
+            if (ComboSorting.SelectedIndex == 3)
+            {
+                // currentAgents = currentAgents.OrderBy(p => p.Discount).ToList();
+            }
+            if (ComboSorting.SelectedIndex == 4)
+            {
+                // currentAgents = currentAgents.OrderByDescending(p => p.Discount).ToList();
+            }
+            if (ComboSorting.SelectedIndex == 5)
+            {
+                currentAgents = currentAgents.OrderBy(p => p.Priority).ToList();
+            }
+            if (ComboSorting.SelectedIndex == 6)
+            {
+                currentAgents = currentAgents.OrderByDescending(p => p.Priority).ToList();
+            }
+
+            if (ComboFilter.SelectedIndex > 0) // Если выбран не "Все типы"
+            {
+                var selectedType = ((ComboBoxItem)ComboFilter.SelectedItem).Content.ToString();
+                currentAgents = currentAgents.Where(p => p.AgentType.Title == selectedType).ToList();
+            }
+
+            AgentListView.ItemsSource = currentAgents.ToList();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+
+        private void ComboFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void ComboSorting_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
         }
     }
 }
