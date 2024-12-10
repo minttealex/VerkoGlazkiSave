1d0884361f58c08be61f6a96fd5d587b6eeb7a8d
Alexandra Verko <minttealex@gmail.com>
2024-12-10 11:52:39 +0500

    12 лаба (нумерация страниц)

--- VerkoGlazkiSave/AgentPage.xaml.cs
+++ VerkoGlazkiSave/AgentPage.xaml.cs
@@ -20,6 +20,13 @@ namespace VerkoGlazkiSave
     /// </summary>
     public partial class AgentPage : Page
     {
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+
+        List<Agent> CurrentPageList = new List<Agent>();
+        List<Agent> TableList;
+
         public AgentPage()
         {
             InitializeComponent();
@@ -89,7 +96,98 @@ namespace VerkoGlazkiSave
                 currentAgents = currentAgents.Where(p => p.AgentType.Title == selectedType).ToList();
             }
 
-            AgentListView.ItemsSource = currentAgents.ToList();
+            AgentListView.ItemsSource = currentAgents;
+
+            TableList = currentAgents;
+
+            ChangePage(0, 0);
+        }
+
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+
+            if (CountRecords % 10 > 0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+
+            Boolean ifUpdate = true;
+
+            int min;
+
+            if (selectedPage.HasValue)
+            {
+                if (selectedPage >= 0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage * 10; i < min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch (direction)
+                {
+                    case 1:
+                        if (CurrentPage > 0)
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            ifUpdate = false;
+                        }
+                        break;
+
+                    case 2:
+                        if (CurrentPage < CountPage - 1)
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            ifUpdate = false;
+                        }
+                        break;
+                }
+            }
+
+                if (ifUpdate)
+                {
+                    PageListBox.Items.Clear();
+                    for (int i = 1; i <= CountPage; i++)
+                    {
+                        PageListBox.Items.Add(i);
+                    }
+                    PageListBox.SelectedIndex = CurrentPage;
+
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    TBCount.Text = min.ToString();
+                    TBAllRecords.Text = " из " + CountRecords.ToString();
+
+                    AgentListView.ItemsSource = CurrentPageList;
+                    AgentListView.Items.Refresh();
+                }
+            
         }
 
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
@@ -106,6 +204,21 @@ namespace VerkoGlazkiSave
         private void ComboSorting_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             UpdateAgents();
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
         }
     }
 }
